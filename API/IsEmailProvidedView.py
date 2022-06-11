c75e86c32e419f117562d981acc0e09998e8b4d8
way2abubakar <45737079+way2abubakar@users.noreply.github.com>
2022-06-11 19:21:00 +0500

    Add files via upload

---
+++ API/IsEmailProvidedView.py
@@ -0,0 +1,38 @@
+import json
+import requests
+# API Email Provided Testing done by Abubakar Siddique
+def send_req_api(url, feature, scenario, given, when):
+    print(
+        "Feature: {}\n\
+        Scenario: {}\n\
+        Given: {}\n\
+        When: {}\n".format(feature,scenario + ":" + url,given,when), end="")
+    try:
+        response = requests.get(url)
+        print("\
+        Then: {}\n\
+        Response Status Code: {}\n\
+        Response Data:\n\t{}\n".format(
+        "Test Passed.",
+        str(response.status_code),
+        str(response.json())
+        ))
+    except Exception as e:
+        print("\
+        Then: {}\n\
+        Response Status Code: {}\n".format(
+        "Test Failed.",
+        str(response.status_code),
+        ))
+
+BASE_URL = "https://rateer.pythonanywhere.com/"
+API_ENDPOINT = "isemailprovided"
+
+url = BASE_URL + API_ENDPOINT + "?" + "username=Clark_Kent"
+send_req_api(url, "IsEmailProvidedView", "Testing API endpoint url", "valid username", "Sending requst to url")
+
+url = BASE_URL + API_ENDPOINT + "?" + "username=asdas"
+send_req_api(url, "IsEmailProvidedView", "Testing API endpoint url", "invalid username", "Sending requst to url")
+
+url = BASE_URL + API_ENDPOINT + "?" + "username="
+send_req_api(url, "IsEmailProvidedView", "Testing API endpoint url", "empty username", "Sending requst to url")
